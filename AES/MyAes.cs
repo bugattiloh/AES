0b26b47d6a29af05124518cd476cabd7305872e9
bugattiloh <bugattiloh2000@gmail.com>
2022-04-14 00:31:07 +0300

    Initial commit

---
+++ AES/MyAes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AES
+{
+    public class MyAes
+    {
+        private readonly AesCryptoServiceProvider _provider;
+
+        public MyAes()
+        {
+            _provider = new AesCryptoServiceProvider {BlockSize = 128, KeySize = 256};
+            _provider.GenerateKey();
+            _provider.GenerateIV();
+            _provider.Mode = CipherMode.CBC;
+            _provider.Padding = PaddingMode.PKCS7;
+        }
+
+        public string Encrypt(string str)
+        {
+            var encryptor = _provider.CreateEncryptor();
+
+            var encryptedStrToByte = encryptor.TransformFinalBlock(Encoding.ASCII.GetBytes(str), 0, str.Length);
+            var res = Convert.ToBase64String(encryptedStrToByte);
+            return res;
+        }
+
+        public String Decrypt(string str)
+        {
+            var decryptor = _provider.CreateDecryptor();
+
+            var encryptedStrToByte = Convert.FromBase64String(str);
+            var decryptedBytes = decryptor.TransformFinalBlock(encryptedStrToByte, 0, encryptedStrToByte.Length);
+
+            var res = Encoding.ASCII.GetString(decryptedBytes);
+            return res;
+        }
+    }
+}
---
+++ AES/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AES
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            var aes = new MyAes();
+            var plaintext = Console.ReadLine();
+            var encryptedText = aes.Encrypt(plaintext);
+            Console.WriteLine(encryptedText);
+            Console.WriteLine(aes.Decrypt(encryptedText));
+        }
+    }
+}
