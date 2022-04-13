91a3b94ecba4e75cfe28f510ac4df923de08be1a
bugattiloh <bugattiloh2000@gmail.com>
2022-04-14 00:39:29 +0300

    ui fixes

--- AES/Program.cs
+++ AES/Program.cs
@@ -9,10 +9,11 @@ namespace AES
         private static void Main(string[] args)
         {
             var aes = new MyAes();
+            Console.Write("Введите текс,который хотели бы зашифровать:");
             var plaintext = Console.ReadLine();
             var encryptedText = aes.Encrypt(plaintext);
-            Console.WriteLine(encryptedText);
-            Console.WriteLine(aes.Decrypt(encryptedText));
+            Console.WriteLine($"Зашифрофанный текст: {encryptedText}");
+            Console.WriteLine($"Текст,расшифрованный обратно: {aes.Decrypt(encryptedText)}");
         }
     }
 }
